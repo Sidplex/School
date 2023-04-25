b3aa312bb3cc336f06472e4ead942e22d5d34753
Siddharth Sahu <74717951+Sidplex@users.noreply.github.com>
2023-04-25 11:09:09 +0530

    Update Reportcardmanagement.py

--- Reportcardmanagement.py
+++ Reportcardmanagement.py
@@ -1,112 +1,405 @@
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
+import matplotlib.pyplot as plt
+import numpy as np
 
+###For Microsoft SQL
+#import pyodbc
 window = Tk()
-window.geometry("1200x500")
+window.geometry("950x500")
 window.title("Student Details")
+window.configure(bg='light blue')
 
 
-def view():
-    import mysql.connector
-    conn = mysql.connector.connect(host='localhost',
-                             database='student',
-                             user='root',
-                             password='bks12345')
-    mycur = conn.cursor()
-    mycur.execute("select * from studentdetails")
-    print("Records shown are: ")
-    for x in mycur:
-        print(x)
-                  
-    mycur.close    
-    conn.close
-
-def submit():
-    import mysql.connector
-    conn = mysql.connector.connect(host='localhost',
-                             database='student',
-                             user='root',
-                             password='bks12345')
-    mycur = conn.cursor()
+try:
+    def view():
     
-    mycur.execute("insert into studentdetails values ('"+ str(entry1.get())+"',"+entry2.get() +","+combo.get() +","+entry4.get() +","+entry5.get() +","+entry6.get() +","+entry7.get() +","+entry8.get() +","+entry9.get()+")")
-    print("Record inserted.")
-    mycur.close
-    mycur.execute("select * from studentdetails ")
-    print("View Total Record:")
-    for x in mycur:
-         print(x)
-    mycur.close
-    conn.commit()     
-    conn.close
-
-def close (): 
-
-    window.destroy()
-    
-def delete():
-    messagebox.showinfo('Message title', 'Message content')
-    import mysql.connector
-    conn = mysql.connector.connect(host='localhost',
-                             database='student',
-                             user='root',
-                             password='bks12345')    
-    mycur = conn.cursor()
-    mycur.execute("delete from studentdetails where rollno="+entry10.get()+"")
-    print("Record deleted")
-    mycur.close
-    mycur.execute("select * from studentdetails ")
-    print("View Total Record:")
-    for x in mycur:
-         print(x)
-    mycur.close
-    conn.commit()     
-    conn.close
-    
-def done():
-    entryy= Entry(window)
-    entryy.grid(row=33,column=5)
-    entryy2= Entry(window)
-    entryy2.grid(row=33,column=7)
-    entryy3= Entry(window)
-    entryy3.grid(row=33,column=9)
-    entryy4= Entry(window)
-    entryy4.grid(row=33,column=11)
-    entryy5= Entry(window)
-    entryy5.grid(row=33,column=13)
+    ###For SQL 5.5.60'''    
+        import mysql.connector
+        conn = mysql.connector.connect(host='localhost',
+                                 database='student',
+                                 user='root',
+                                 password='')
+        mycur = conn.cursor()
+        mycur.execute("select * from studentdetails")
+        print("Records shown are: ")
+        for x in mycur:
+            print(x)
+                      
+        mycur.close    
+        conn.close
     
     
-def edit():
-    labell = Label(window,text="What do you want to edit?")
-    labell.grid(row=31,column=10,command=done)
-    btton = Button(text="ENG")
-    btton.grid(row=32,column=5)
-    btton2 = Button(text="MATH",command=done)
-    btton2.grid(row=32,column=7)
-    btton3 = Button(text="SCIENCE",command=done)
-    btton3.grid(row=32,column=9)
-    btton4 = Button(text="CS",command=done)
-    btton4.grid(row=32,column=11)
-    btton5 = Button(text="SSC",command=done)
-    btton5.grid(row=32,column=13)
+    ###For Microsoft SQL'''
+    #    conn = pyodbc.connect("Driver={SQL Server};"
+    #                        "Server=LAPTOP-3QC3HN03\SQLEXPRESS;"
+    #                        "database=student;"
+    #                        "uid=knsa;pwd=knrai")
+    #    mycur = conn.cursor()
+    #    mycur.execute("select * from student.dbo.studentdetails")
+    #    print("Records shown are: ")
+    #    for x in mycur:
+    #        print(x)
+    #                  
+    #    mycur.close    
+    #    conn.close
     
-#        import mysql.connector
-#        conn = mysql.connector.connect(host='localhost',
-#                                 database='student',
-#                                 user='root',
-#                                 password='bks12345')
-#        mycur = conn.cursor()
-#        mycur.execute("alter table")
-#        entryy=Entry(window)
-#        entryy.grid(row=31,column=10)
-
-
-
-
-
     
-label1 = Label(window,text="School Name- Amity International School",font="Arial 15")
+    def submit():
+    
+    ###For SQL 5.5.60'''    
+        import mysql.connector
+        conn = mysql.connector.connect(host='localhost',
+                                 database='student',
+                                 user='root',
+                                 password='')
+        mycur = conn.cursor()
+        
+        mycur.execute("insert into studentdetails values ('"+ str(entry1.get())+"',"+entry2.get() +","+combo.get() +","+entry4.get() +","+entry5.get() +","+entry6.get() +","+entry7.get() +","+entry8.get() +","+entry9.get()+")")
+        print("Record inserted.")
+        mycur.close
+        mycur.execute("select * from studentdetails ")
+        print("View Total Record:")
+        for x in mycur:
+             print(x)
+        mycur.close
+        conn.commit()     
+        conn.close
+        
+    ###For Microsoft SQL'''
+    #    conn = pyodbc.connect("Driver={SQL Server};"
+    #                        "Server=LAPTOP-3QC3HN03\SQLEXPRESS;"
+    #                        "database=student;"
+    #                        "uid=knsa;pwd=knrai")
+    #    mycur = conn.cursor()
+    #    #mycur.execute("insert into studentdetails values(  str(entry1.get()),int(entry2.get()), int(entry3.get()), int(entry4.get()), int(entry5.get()), int(entry6.get()), int(entry7.get()), int(entry8.get()), int(entry9.get())    )")
+    #    #mycur.execute("insert into studentdetails values('Sid',5814,67,77,97,85,68,300,777)")
+    #    #print("insert into studentdetails values ('"+ str(entry1.get())+"',"+entry2.get() +","+entry3.get() +","+entry4.get() +","+entry5.get() +","+entry6.get() +","+entry7.get() +","+entry8.get() +","+entry9.get() +")")
+    #    mycur.execute("insert into student.dbo.studentdetails values ('"+ str(entry1.get())+"',"+entry2.get() +","+combo.get() +","+entry4.get() +","+entry5.get() +","+entry6.get() +","+entry7.get() +","+entry8.get() +","+entry9.get()+")")
+    #    print("Record inserted.")
+    #    mycur.close
+    #    mycur.execute("select * from student.dbo.studentdetails ")
+    #    print("View Total Record:")
+    #    for x in mycur:
+    #         print(x)
+    #    mycur.close
+    #    conn.commit()     
+    #    conn.close
+    
+    
+    def close (): 
+    
+        window.destroy()
+        
+    def delete():
+        messagebox.showinfo('Delete', 'Do you want to delete?')
+    ##For SQL 5.5.60'''
+        import mysql.connector
+        conn = mysql.connector.connect(host='localhost',
+                                 database='student',
+                                 user='root',
+                                 password='')    
+        mycur = conn.cursor()
+        mycur.execute("delete from studentdetails where rollno="+entry10.get()+"")
+        print("Record deleted")
+        mycur.close
+        mycur.execute("select * from studentdetails ")
+        print("View Total Record:")
+        for x in mycur:
+             print(x)
+        mycur.close
+        conn.commit()     
+        conn.close
+    
+    
+    ###For Microsoft SQL   
+    #    conn = pyodbc.connect("Driver={SQL Server};"
+    #                        "Server=LAPTOP-3QC3HN03\SQLEXPRESS;"
+    #                        "database=student;"
+    #                        "uid=knsa;pwd=knrai")
+    #    mycur = conn.cursor()
+    #    mycur.execute("delete from student.dbo.studentdetails where rollno="+entry10.get()+"")
+    #    print("Record deleted")
+    #    mycur.close
+    #    mycur.execute("select * from student.dbo.studentdetails ")
+    #    print("View Total Record:")
+    #    for x in mycur:
+    #         print(x)
+    #    mycur.close
+    #    conn.commit()     
+    #    conn.close
+    
+
+    def bargraph():
+        graph = Toplevel()
+        graph.geometry("500x500")
+        graph.title("Bargraph")
+
+        import mysql.connector
+        conn = mysql.connector.connect(host='localhost',
+                                 database='student',
+                                 user='root',
+                                 password='')    
+        mycur = conn.cursor()
+        mycur.execute("select engmarks, mathmarks, scimarks, csmarks, sscmarks from studentdetails")
+        v=mycur.fetchall()
+        l2=[]
+        for x in v:
+            y=x
+            l=list(y)
+            l2.append(l)
+ 
+        eng=[]
+        math=[]
+        sci=[]
+        cs=[]
+        ssc=[]
+        for lol in range(len(l2)):
+            eng.append(l2[lol][0])
+            math.append(l2[lol][1])
+            sci.append(l2[lol][2])
+            cs.append(l2[lol][3])
+            ssc.append(l2[lol][4])
+
+        
+        mycur.close
+        conn.commit()     
+        conn.close  
+        import mysql.connector
+        conn = mysql.connector.connect(host='localhost',
+                                 database='student',
+                                 user='root',
+                                 password='')    
+        mycur = conn.cursor()
+        mycur.execute("select studname from studentdetails")
+        print("View Total Record:")
+        l3=[]
+        for name in mycur:
+            l=name
+            s=list(l)
+            l3.append(s)
+
+            
+        mycur.close
+        conn.commit()     
+        conn.close
+
+        
+        n_groups=len(eng)
+        fig, ax = plt.subplots()
+        index=np.arange(n_groups)
+        bar_width = 0.1
+        opacity = 0.8
+        plt.bar(index,eng,bar_width,alpha=opacity,color='b',label='English')
+        plt.bar(index + bar_width, math, bar_width,alpha=opacity,color='g',label='math')
+        plt.bar(index + (2*bar_width), sci, bar_width,alpha=opacity,color='r',label='science')
+        plt.bar(index + (3*bar_width), cs, bar_width,alpha=opacity,color='m',label='CS')
+        plt.bar(index + (4*bar_width), ssc, bar_width,alpha=opacity,color='y',label='SSC')
+        plt.xlabel('Person')
+        plt.ylabel('Scores')
+        plt.title('Scores by person')
+        plt.xticks(index + bar_width,l3)
+        plt.legend()
+        
+        plt.tight_layout()
+        plt.savefig("testbar.png")
+
+        photo = PhotoImage(file= r"testbar.png")
+        labbb = Label(graph,image = photo)
+        labbb.pack(side = TOP)
+        
+        graph.mainloop()
+                
+    
+    def edit():
+        
+        main = Tk()
+        main.geometry("650x500")
+        main.title("Edit menu")
+        main.configure(bg="light blue")                
+
+        labb = Label(main,text="Marks Updation",font="Arial 15")
+        labb.grid(row=1,column=4)
+        def eng():
+            import mysql.connector
+            conn = mysql.connector.connect(host='localhost',
+                                     database='student',
+                                     user='root',
+                                     password='')    
+            mycur = conn.cursor()
+            mycur.execute("update studentdetails set engmarks="+entryy.get()+" where rollno="+roll1.get()+";")
+            print("Record edited")
+            mycur.close
+            mycur.execute("select * from studentdetails ")
+            print("View Total Record:")
+            for x in mycur:
+                 print(x)
+            mycur.close
+            conn.commit()     
+            conn.close
+        def math():
+            import mysql.connector
+            conn = mysql.connector.connect(host='localhost',
+                                     database='student',
+                                     user='root',
+                                     password='')    
+            mycur = conn.cursor()
+            mycur.execute("update studentdetails set mathmarks="+entryy2.get()+" where rollno="+roll1.get()+";")
+            print("Record edited")
+            mycur.close
+            mycur.execute("select * from studentdetails ")
+            print("View Total Record:")
+            for x in mycur:
+                 print(x)
+            mycur.close
+            conn.commit()     
+            conn.close
+        def sci():
+            import mysql.connector
+            conn = mysql.connector.connect(host='localhost',
+                                     database='student',
+                                     user='root',
+                                     password='')    
+            mycur = conn.cursor()
+            mycur.execute("update studentdetails set scimarks="+entryy3.get()+" where rollno="+roll1.get()+";")
+            print("Record edited")
+            mycur.close
+            mycur.execute("select * from studentdetails ")
+            print("View Total Record:")
+            for x in mycur:
+                 print(x)
+            mycur.close
+            conn.commit()     
+            conn.close
+        def csmarks():
+            import mysql.connector
+            conn = mysql.connector.connect(host='localhost',
+                                     database='student',
+                                     user='root',
+                                     password='')    
+            mycur = conn.cursor()
+            mycur.execute("update studentdetails set csmarks="+entryy4.get()+" where rollno="+roll1.get()+";")
+            print("Record edited")
+            mycur.close
+            mycur.execute("select * from studentdetails ")
+            print("View Total Record:")
+            for x in mycur:
+                 print(x)
+            mycur.close
+            conn.commit()     
+            conn.close
+        def sscmarks():
+            import mysql.connector
+            conn = mysql.connector.connect(host='localhost',
+                                     database='student',
+                                     user='root',
+                                     password='')    
+            mycur = conn.cursor()
+            mycur.execute("update studentdetails set sscmarks="+entryy5.get()+" where rollno="+roll1.get()+";")
+            print("Record edited")
+            mycur.close
+            mycur.execute("select * from studentdetails ")
+            print("View Total Record:")
+            for x in mycur:
+                 print(x)
+            mycur.close
+            conn.commit()     
+            conn.close
+            
+
+        
+        
+        btton = Button(main,text="EDIT ENG",command=eng)
+        btton.grid(row=3,column=4)
+        
+        entryy= Entry(main)
+        entryy.grid(row=3,column=3)
+        
+        lab1 = Label(main,text="Enter new english marks ->")
+        lab1.grid(row=3,column=2)
+        
+        roll1 = Entry(main)
+        roll1.grid(row=2,column=4)
+        
+        lab= Label(main,text="Enter Roll no.->")
+        lab.grid(row=2,column=3)
+        
+        but = Button(main,text = "ENTER")
+        but.grid(row=2,column=5)
+        
+        btton2 = Button(main,text="EDIT MATH",command=math)
+        btton2.grid(row=4,column=4)
+        
+        entryy2=Entry(main)
+        entryy2.grid(row=4,column=3)
+        
+        lab2 = Label(main,text="Enter new math marks ->")
+        lab2.grid(row=4,column=2)
+        
+        btton3 = Button(main,text="EDIT SCIENCE",command=sci)
+        btton3.grid(row=5,column=4)
+        
+        entryy3=Entry(main)
+        entryy3.grid(row=5,column=3)
+        
+        lab3 = Label(main,text="Enter new science marks ->")
+        lab3.grid(row=5,column=2)
+        
+        btton4 = Button(main,text="EDIT CS",command=csmarks)
+        btton4.grid(row=6,column=4)
+        
+        entryy4=Entry(main)
+        entryy4.grid(row=6,column=3)
+        
+        lab4 = Label(main,text="Enter new CS marks ->")
+        lab4.grid(row=6,column=2)
+        
+        btton5 = Button(main,text="EDIT SSC",command=sscmarks)
+        btton5.grid(row=7,column=4)
+        
+        entryy5=Entry(main)
+        entryy5.grid(row=7,column=3)
+        
+        lab5 = Label(main,text="Enter new SSC marks ->")
+        lab5.grid(row=7,column=2)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        main.mainloop()
+    
+    ###For SQL 5.5.60    
+        import mysql.connector
+        conn = mysql.connector.connect(host='localhost',
+                                 database='student',
+                                 user='root',
+                                 password='')
+        mycur = conn.cursor()
+        mycur.execute("alter table")
+        entryy=Entry(window)
+        entryy.grid(row=31,column=10)
+    
+    ###For Microsoft SQL
+    #        conn1 = pyodbc.connect("Driver={SQL Server};"
+    #                            "Server=LAPTOP-3QC3HN03\SQLEXPRESS;"
+    #                            "database=student;"
+    #                            "uid=knsa;pwd=knrai")
+    #        mycur1 = conn1.cursor()
+    #        mycur1.execute("alter table")
+    #        entryy=Entry(window)
+    #        entryy.grid(row=32,column=10)
+except:
+    print()
+label1 = Label(window,text="Amity International School",font="Castellar 15")
 label1.grid(row=3,column=10)
 entry1 = Entry(window)
 entry1.grid(row=4,column=1)
@@ -119,7 +412,7 @@ entry2.grid(row=5,column=1)
 label3 = Label(window,text="Roll no:")
 label3.grid(row=5,column=0)
 combo = Combobox (window)
-combo['values']= (8, 9, 10, 11, 12, "Select class:")
+combo['values']= (8, 9, 10, 11, 12, "Select")
 combo.current(5)
 combo.grid(row=6, column=1)
 
@@ -177,10 +470,13 @@ entry10 = Entry(window)
 entry10.grid(row=29,column=15)
 
 bt7 = Button(text="Edit",command=edit)
-bt7.grid(row=30,column=2)
-label13 = Label(window,text="Insert to edit table--->")
-label13.grid(row=30,column=10)
-entry11 = Entry(window)
-entry11.grid(row=30,column=15)
+bt7.grid(row=30,column=10)
+
+btt10 = Button(text="Show Graph",command=bargraph)
+btt10.grid(row=31,column=10)
+
 window.mainloop()
+
+
+
 
